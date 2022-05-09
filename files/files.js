dc18c5e07ab75773279cd5ac79d5a5bfa63f47b5
Robert Snow <robjsnow@gmail.com>
2022-05-09 12:44:33 -0500

    added fs examples to files.js

--- files/files.js
+++ files/files.js
@@ -1,1 +1,46 @@
+//Require file system
+//https://nodejs.dev/learn/the-nodejs-fs-module
+
 const fs = require('fs');
+
+//read a file
+fs.readFile('./docs/text1.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(data.toString());
+});
+
+//Write a file
+//If it doesn't exist, it will create the file for you
+//It will replace current content with new content
+fs.writeFile('./docs/text2.txt', 'Hello!', () => {
+    console.log("file written.");
+});
+
+// Directories
+if (!fs.existsSync('./newfolder')) {
+fs.mkdir('./newfolder', (err => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('folder created.');
+}));
+} else {
+    fs.rmdir('./newfolder', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder deleted.');
+    })
+}
+
+// Deleting files
+if (fs.existsSync("./docs/delete.txt")) {
+    fs.unlink('./docs/delete.txt', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('file deleted.');
+    })
+}
