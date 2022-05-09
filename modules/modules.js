375a0836d817b409582dfe311f168c9901c8bb67
Robert Snow <robjsnow@gmail.com>
2022-05-09 11:25:48 -0500

    added modules.js

---
+++ modules/modules.js
@@ -0,0 +1,17 @@
+//vehicles.js code is executed, and then vehicles is assigned an empty object {}
+const vehicleTypes = require("./vehicles");
+
+//pets.js code is executed, then petData is assigned {names, ages} object
+//because of the module.exports line on pets.js file
+const petData = require("./pets");
+
+console.log(petData.names);
+console.log(petData.ages);
+
+//We could just get the names of petData instead
+const { names } = require("./pets");
+console.log(names);
+
+//Example of requiring the built-in os core module
+const os = require("os");
+console.log(os.platform(), os.homedir());
